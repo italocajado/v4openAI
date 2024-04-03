48b42f36125208cf2d9f15d2e9dc999a72289543
italocajado <italocajado@gmail.com>
2024-04-02 22:59:01 -0300

    Ajustes no codigo

--- v4.py
+++ v4.py
@@ -5,6 +5,8 @@ from PIL import Image, ImageTk
 from openai import OpenAI
 import pyttsx3
 
+client = OpenAI(api_key='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')
+
 class CameraApp:
     def __init__(self, master):
         self.master = master
@@ -12,18 +14,15 @@ class CameraApp:
 
         # Inicializando a câmera
         self.cap = cv2.VideoCapture(0)
-
         self.canvas = Canvas(master, width=640, height=480)
         self.canvas.pack()
-
         self.update()
 
-        # Criando um botão para tirar a foto
+        # botão para tirar a foto
         self.btn_capture = Button(master, text="Capturar", command=self.capture)
         self.btn_capture.pack()
 
     def update(self):
-        # Capturando a imagem da câmera
         ret, frame = self.cap.read()
 
         if ret:
@@ -36,16 +35,12 @@ class CameraApp:
         self.master.after(10, self.update)
 
     def capture(self):
-    # Capturando a imagem da câmera
       ret, frame = self.cap.read()
 
       if ret:
         # Salvando a imagem capturada
         cv2.imwrite("temp_image.jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
-
-        # Finalizando a câmera
         self.cap.release()
-
         # Chamando a função para enviar a imagem para descrição
         self.send_image()
 
@@ -57,10 +52,7 @@ class CameraApp:
 
         # Criando a URL da imagem
         image_url = "data:image/jpeg;base64," + encoded_string
-
-        # Criando cliente OpenAI
-        client = OpenAI(api_key='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')
-
+        
         # Enviando a imagem para descrição
         response = client.chat.completions.create(
             model="gpt-4-vision-preview",
@@ -91,6 +83,7 @@ class CameraApp:
     def convert_text_to_speech(self, text):
         engine = pyttsx3.init()
         engine.say(text)
+        engine.setProperty('rate', 1.5)
         engine.runAndWait()
 
 root = Tk()
